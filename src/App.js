762fee8d4d7594886e4f2b21e4cb9e6613c2a113
Fernando <fernando.isdicoder@gmail.com>
2021-11-29 18:40:20 +0100

    Added in app oneSwimmer and home routes

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import SwimmerForm from "./components/SwimmerForm/SwimmerForm";
 import SwimmersPage from "./pages/SwimmersPage";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import OneSwimmerPage from "./pages/OneSwimmerPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="out-box">
-        <div className="inside-box">
-          <BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <div className="out-box">
+          <div className="inside-box">
             <Routes>
+              <Route path={path.home} element={<HomePage />}></Route>
               <Route path={path.register} element={<SwimmerForm />}></Route>
               <Route path={path.swimmers} element={<SwimmersPage />}></Route>
+              <Route path={path.oneSwimmer} element={<OneSwimmerPage />} />
             </Routes>
-          </BrowserRouter>
+          </div>
         </div>
-      </div>
-      <Footer />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
