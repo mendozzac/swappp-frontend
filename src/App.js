5d25ea4d6026ee737f4a05ae7bdcccc8728a0938
Fernando <fernando.isdicoder@gmail.com>
2021-11-29 09:37:47 +0100

    Sass: Colors variables and extends to reorganize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import path from "../src/path/path";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/media.scss";
 import "./App.scss";
 import SwimmerForm from "./components/SwimmerForm/SwimmerForm";
 import SwimmersPage from "./pages/SwimmersPage";
@@ -10,12 +11,16 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path={path.register} element={<SwimmerForm />}></Route>
-          <Route path={path.swimmers} element={<SwimmersPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <div className="out-box">
+        <div className="inside-box">
+          <BrowserRouter>
+            <Routes>
+              <Route path={path.register} element={<SwimmerForm />}></Route>
+              <Route path={path.swimmers} element={<SwimmersPage />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
       <Footer />
     </div>
   );
