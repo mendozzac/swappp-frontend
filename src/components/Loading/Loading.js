2e496fbccaf4da8e29693f5f056e9be921f755a4
Fernando <fernando.isdicoder@gmail.com>
2021-11-30 15:03:53 +0100

    Added loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading-box">
+      <h1 className="title">Cargando...</h1>
+      <img className="logo" src="/img/swappp.svg" width="80%" alt="Cargando" />
+    </div>
+  );
+};
+
+export default Loading;
