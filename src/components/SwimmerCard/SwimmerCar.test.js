eb15a448d36accb168cfa54b994bb87d980c4849
Fernando <fernando.isdicoder@gmail.com>
2021-12-02 16:47:51 +0100

    Added SwimmerDetail styles

--- src/components/SwimmerCard/SwimmerCar.test.js
+++ src/components/SwimmerCard/SwimmerCar.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import SwimmerCard from "./SwimmerCard";
-import ReactTestRenderer from "react-test-renderer";
 import { BrowserRouter } from "react-router-dom";
 
 describe("Given a SwimmerCard component", () => {
@@ -29,31 +28,6 @@ describe("Given a SwimmerCard component", () => {
       const newSwimmer = screen.getByText("Geofrey");
 
       expect(newSwimmer).toBeInTheDocument();
-    });
-  });
-  describe("When it receives an object swimmer", () => {
-    test("Then it should render the swimmer", () => {
-      const swimmer = {
-        name: "Geofrey",
-        surname: "Campbell",
-        birthdate: "11/08/90",
-        height: 190,
-        weight: 80,
-        times: {
-          distance: 200,
-          style: "Espalda",
-          date: "22/09/19",
-          time: "1:56.09",
-          pool: 50,
-        },
-      };
-
-      const newSwimmer = ReactTestRenderer.create(
-        <BrowserRouter>
-          <SwimmerCard swimmer={swimmer} />
-        </BrowserRouter>
-      );
-      expect(newSwimmer.toJSON()).toMatchSnapshot();
     });
   });
 });
