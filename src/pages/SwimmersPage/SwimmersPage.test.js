66f0c4b3c6fdae01889a1ee1b9d99019af5bc70d
Fernando <fernando.isdicoder@gmail.com>
2021-12-03 11:30:41 +0100

    Fix what render SwimmersPage test

--- src/pages/SwimmersPage/SwimmersPage.test.js
+++ src/pages/SwimmersPage/SwimmersPage.test.js
@@ -1,15 +1,15 @@
 import { screen, render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import SwimmersList from "../../components/SwimmersList/SwimmersList";
 import configureStore from "../../redux/store/index";
+import SwimmersPage from "./SwimmersPage";
 
-describe("Given a SwimmerPage", () => {
+describe("Given a SwimmersPage", () => {
   describe("When it renders", () => {
     test("Then it should render a title", () => {
       const store = configureStore();
       render(
         <Provider store={store}>
-          <SwimmersList />
+          <SwimmersPage />
         </Provider>
       );
       const title = screen.getByText("Cargando...");
