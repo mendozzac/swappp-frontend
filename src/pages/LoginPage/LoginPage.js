28c652883fd47e05ebb7ca14dbe293ea5b32e386
Fernando <fernando.isdicoder@gmail.com>
2021-12-06 16:21:37 +0100

    Added restriction to acces if is coach or not

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -11,7 +11,7 @@ const LoginPage = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    isAuth ? navigate(path.swimmers) : <Loading />;
+    isAuth ? navigate(path.season) : <Loading />;
   });
   return <LoginForm />;
 };
---
+++ src/pages/SeasonPage/SeasonPage.js
@@ -0,0 +1,13 @@
+const SeasonPage = () => {
+  return (
+    <>
+      <h2>22/11 - </h2>
+      <h2>23/11 - </h2>
+      <h2>24/11 - </h2>
+      <h2>25/11 - </h2>
+      <h2>26/11 - </h2>
+    </>
+  );
+};
+
+export default SeasonPage;
